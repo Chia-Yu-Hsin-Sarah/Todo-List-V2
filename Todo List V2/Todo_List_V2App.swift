8ec4ede723a6362a4ed515eb514de94d2e89fd45
Chia Yu Hsin Sarah <sarah.chia8795@gmail.com>
2023-08-05 09:36:47 +0800

    Initial Commit

---
+++ Todo List V2/Todo_List_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_V2App.swift
+//  Todo List V2
+//
+//  Created by Sarah Chia on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
